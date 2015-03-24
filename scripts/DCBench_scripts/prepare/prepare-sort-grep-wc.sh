7b6aa8c817b7503e065e4de013a15ce2777b895a
Mohammad Alian <alian@optiplexXE2>
2015-03-24 12:29:17 -0500

    updated prepare script for sort,grep,wc - now we just need to run one prepare script and take one checkpoint for all these 3 benchmarks

--- scripts/DCBench_scripts/prepare/prepare-sort-grep-wc.sh
+++ scripts/DCBench_scripts/prepare/prepare-sort-grep-wc.sh
@@ -6,8 +6,8 @@ export HIVE_HOME=/root/apache-hive-0.13.1-bin
 export MAHOUT_HOME=/root/mahout-distribution-0.6
 
 
-cd /benchmark/DCBench/workloads/base-operations/wordcount
-./prepare-wordcount.sh 100MB
+cd /benchmarks/DCBench/workloads/base-operations/grep
+./prepare-grep.sh 120MB
 
 
 /sbin/m5 checkpoint 0 0
